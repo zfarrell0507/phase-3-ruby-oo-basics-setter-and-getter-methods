c293850dd2559da9e5a7c8e0de95ddf9d39d8444
Zachary Farrell <zfarrell0507@gmail.com>
2022-09-06 12:26:29 -0400

    commit tree 2a1a9b97017adbb6ebb074d3c0771ca53fa099f9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+    attr_reader :name, :breed
+    attr_writer :name, :breed
+    def name
+        @name=(name)
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed=(breed)
+    end
+    def breed
+        @breed
+    end
 end
+d1 = Dog.new
+d1.name = 'Natalie'
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-
+    attr_reader :name, :job
+    attr_writer :name, :job
+    def name
+        @name=(name)
+    end
+    def name
+        @name
+    end
+    def job
+        @job=(job)
+    end
+    def job
+        @job
+    end
 end
